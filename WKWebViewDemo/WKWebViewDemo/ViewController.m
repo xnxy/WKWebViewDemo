15eeabf415627510eb7fb14a3b530984b194bad7
周伟 <2303400083@qq.com>
2018-12-04 11:50:00 +0800

    测试

--- WKWebViewDemo/WKWebViewDemo/ViewController.m
+++ WKWebViewDemo/WKWebViewDemo/ViewController.m
@@ -34,17 +34,19 @@
     self.webView = webView;
     [self.view addSubview:webView];
     
-    NSString *filePath = [[NSBundle mainBundle] pathForResource:@"index" ofType:@"html"];
+    NSString *filePath = [[NSBundle mainBundle] pathForResource:@"test" ofType:@"html"];
     NSURL *baseURL = [[NSBundle mainBundle]bundleURL];
-    
+
     [webView loadHTMLString:[NSString stringWithContentsOfFile:filePath encoding:NSUTF8StringEncoding error:nil] baseURL:baseURL];
+    
+//    NSURL *url = [NSURL URLWithString:@"……"];
+//
+//    [webView loadRequest:[NSURLRequest requestWithURL:url]];
     
     WKUserContentController *userContentController = config.userContentController;
     
     //JS调用OC代码  添加处理脚本
-    [userContentController addScriptMessageHandler:self name:@"showMobile"];
-    [userContentController addScriptMessageHandler:self name:@"showName"];
-    [userContentController addScriptMessageHandler:self name:@"showSendMsg"];
+    [userContentController addScriptMessageHandler:self name:@"test"];
     
     //layout
     [webView makeConstraints:^(MASConstraintMaker *make) {
@@ -54,6 +56,22 @@
     
     NSLog(@"----title:%@---URL:%@---",self.webView.title,self.webView.URL);
     
+    UIButton *btn = [UIButton buttonWithType:UIButtonTypeCustom];
+    btn.backgroundColor = [UIColor blueColor];
+    [self.view addSubview:btn];
+    
+    [btn makeConstraints:^(MASConstraintMaker *make) {
+        make.centerX.equalTo(self.view);
+        make.bottom.equalTo(self.bottomLayoutGuide).offset(-20);
+        make.width.height.equalTo(100);
+    }];
+    
+    @weakify(self)
+    [btn bk_whenTapped:^{
+        @strongify(self)
+        [self getDeviceInfo];
+    }];
+    
 }
 
 #pragma mark -
@@ -61,19 +79,16 @@
 - (void)userContentController:(WKUserContentController *)userContentController didReceiveScriptMessage:(WKScriptMessage *)message{
     
     NSLog(@"-----message:%@-----name:%@-----body:%@-----",message,message.name,message.body);
-    
-    if ([message.name isEqualToString:@"showMobile"]) {
-        
-    }
-    
-    if ([message.name isEqualToString:@"showName"]) {
-        
-    }
-    
-    if ([message.name isEqualToString:@"showSendMsg"]) {
-        
-    }
-    
+}
+
+- (void)getDeviceInfo{
+
+    NSDictionary *msg = @{@"msg": @"12345678"
+                          };
+    NSString *jsCode = [NSString stringWithFormat:@"deviceInfo('%@')", [msg yy_modelToJSONString]];
+    [self.webView evaluateJavaScript:jsCode completionHandler:^(id _Nullable response, NSError * _Nullable error) {
+        NSLog(@"response %@", response);
+    }];
 }
 
 
